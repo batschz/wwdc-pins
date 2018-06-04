eec2882ea5be73d00f5c46f89b1215b04f28d298
Franz Busch <privat@franz-busch.de>
2018-06-04 05:47:43 +0200

    lul

---
+++ Pins/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  Pins
+//
+//  Created by franz busch on 04.06.18.
+//  Copyright © 2018 Werner Huber. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet private var nameTextField: UITextField!
+
+    @IBAction private func nextButtonTapped(_ sender: UIButton) {
+        guard let name = nameTextField.text else {
+            return
+        }
+
+        UserService().save(username: name) { error in
+            if let error = error {
+
+            } else {
+                (UIApplication.shared.delegate as? AppDelegate)?.showTradesViewController()
+            }
+        }
+    }
+
+}
+
---
+++ Pins/ViewControllers/TradesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TradesViewController.swift
+//  Pins
+//
+//  Created by franz busch on 04.06.18.
+//  Copyright © 2018 Werner Huber. All rights reserved.
+//
+
+import Foundation
